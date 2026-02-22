a53042a4c9aa04e17f40879b561e324ebecec6e9
Guillermo Hernandez <guillermohernandez@gmail.com>
2026-02-22 14:27:04 -0600

    added object tests and none tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -6,14 +6,18 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
+    
+    def test_uneq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
 
     def test_none(self):
         tester1 = TextNode("This is a text node", TextType.LINK, "github.com")
         self.assertIsNotNone(tester1)
-
-    def test_not_none(self):
-        tester2 = TextNode("This is a text node", TextType.LINK, url = None)
-        self.assertIsNone(tester2)
+    def test_obj(self):
+        tester1 = TextNode("This is a text node", TextType.LINK, "github.com")
+        self.assertIsInstance(tester1, TextNode)
 
 if __name__ == "__main__":
     unittest.main()
